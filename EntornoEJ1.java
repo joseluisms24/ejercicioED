48ca853a2dcf874e4d38aa796dd0f34a7c62672c
joseluisms24 <joseluismedina2019@gmail.com>
2024-09-26 10:25:42 +0200

    Add files via upload

---
+++ EntornoEJ1.java
@@ -0,0 +1,32 @@
+
+package entornoej1;
+import java.util.Scanner;
+/**
+ *
+ * @author usuarioDAW
+ */
+public class EntornoEJ1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dame la distancia a recorrer en kilometros");
+       double distanciaRecorrer = sc.nextDouble();
+        
+        System.out.println("Dame la velocidad promedio esperada en km/h");
+        double velocidad = sc.nextDouble();
+        
+        double horas = distanciaRecorrer / velocidad;
+        
+        System.out.println("Las horas que vas a tardar son: "+horas);
+        
+       
+        
+        
+        
+    }
+    
+}
